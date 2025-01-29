5edf09af49bb39877a331abdf083c2227c345732
ValeriyaKot <valeric.kot988@gmail.com>
2025-01-29 20:54:36 +0300

    Add ChooseButton

--- era-book-app/src/styles/GlobalStyles.js
+++ era-book-app/src/styles/GlobalStyles.js
@@ -3,50 +3,78 @@ import { Platform, StyleSheet, View, TouchableOpacity, Text } from 'react-native
 
 
 export const MainButton = ({ onPress, children, style }) => {
-  return (
-    <TouchableOpacity onPress={onPress} style={[styles.mainButton, style]}>
-      <Text style={styles.mainButtonText}>{children}</Text>
-    </TouchableOpacity>
-  );
+    return (
+        <TouchableOpacity onPress={onPress} style={[styles.mainButton, style]}>
+            <Text style={styles.mainButtonText}>{children}</Text>
+        </TouchableOpacity>
+    );
 };
 
+export const ChooseButton = ({ label, isSelected, onSelect }) => {
+    return (
+        <TouchableOpacity style={[styles.option, isSelected ? styles.activeOption : null]} onPress={onSelect}>
+            <Text style={styles.optionText}>{label}</Text>
+        </TouchableOpacity>
+    );
+};
 
 export const theme = {
-  colors: {
-    primary: '#F89300',
-    secondary: '#03dac6',
-    background: '#FFFFFF',
-    text: '#333',
-    white: '#fff',
-  },
-  fontSizes: {
-    small: 12,
-    medium: 15,
-    large: 24,
-  },
-  spacing: {
-    small: 8,
-    medium: 16,
-    large: 24,
-  },
+    colors: {
+        primary: '#F89300',
+        secondary: '#03dac6',
+        background: '#FFFFFF',
+        text: '#333',
+        white: '#fff',
+    },
+    fontSizes: {
+        small: 12,
+        medium: 15,
+        large: 24,
+    },
+    spacing: {
+        small: 8,
+        medium: 16,
+        large: 24,
+    },
 };
 
 
 const styles = StyleSheet.create({
-  mainButton: {
-    backgroundColor: theme.colors.primary,
-    width: 300,
-    height: 40,
-    borderRadius: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: 10,
-  },
-  mainButtonText: {
-    color: theme.colors.white,
-    fontSize: theme.fontSizes.medium,
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center'
-  },
+    mainButton: {
+        backgroundColor: theme.colors.primary,
+        width: 300,
+        height: 40,
+        borderRadius: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 10,
+    },
+    mainButtonText: {
+        color: theme.colors.white,
+        fontSize: theme.fontSizes.medium,
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center'
+    },
+    option: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'flex-start',
+        borderWidth: 1,
+        borderColor: '#F89300',
+        backgroundColor: '#fff',
+        width: '100%',
+        height: 40,
+        borderRadius: 50,
+        margin: 10,
+        marginLeft: 0,
+    },
+    activeOption: {
+        backgroundColor: '#F89300',
+
+    },
+    optionText: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
 });
