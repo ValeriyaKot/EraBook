08d2bff13fa7192e7a565393f1a9a3f74cc3c5ad
ValeriyaKot <valeric.kot988@gmail.com>
2025-02-02 19:52:47 +0300

    Add apps, add users.model

---
+++ backend/apps/users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.core.validators import MaxValueValidator
+from datetime import datetime
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    full_name = models.TextField(max_length=255)
+    birthday = models.DateField(validators=[MaxValueValidator(datetime.now().date())], blank=True, null=True)
+    phone_number = models.CharField(max_length=20, unique=True)
+    country = models.TextField(max_length=255)
+
+    def __str__(self):
+        return self.user.username
--- backend/era_backend/settings.py
+++ backend/era_backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'apps.users'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'era_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'EraBook',
+        'USER': 'valeriya',
+        'PASSWORD': 'karusel988',
+        'HOST': 'localhost',  # Или IP сервера, если база на удалённом сервере
+        'PORT': '5432',  # Обычный порт для PostgreSQL
     }
 }
 
